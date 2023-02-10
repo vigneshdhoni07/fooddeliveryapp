c8cc35bb05045558105113eac9b23225d0f102af
Vignesh Ravichandran <vigneshr470@gmail.com>
2023-02-10 18:32:27 +0530

    food app all category module

--- Controller/Categorycontroller.js
+++ Controller/Categorycontroller.js
@@ -16,3 +16,81 @@ exports.createcategory=async(req,res)=>{
     }
 
 }
+exports.getallcategories=async(req,res)=>{
+    try{
+    let allcateg=await category.findAll()
+    res.status(200).json(allcateg)
+    
+}
+catch(err)
+{
+    res.status(400).json(err||{message:"something went wrong"})
+}
+    
+}
+
+exports.getonecategory=async(req,res)=>{
+    try {
+        var name=req.params.name
+        var categ=await category.findOne({where:{name:name}})
+        res.status(200).json(categ)
+        
+    } catch (error) {
+        res.status(400).json(err||{message:"something went wrong"})
+    }
+}
+exports.updatecateg=async(req,res)=>{
+    try {
+        if(!req.isAdmin && !req.isVerified)
+        {
+            res.status(400).json({message:"Unauthorised Access"})
+        }
+
+            var id=req.params.id
+            
+            var {name}=req.body
+            var updatedcateg={
+                name:name
+            }
+
+           var updatedcategory= await category.update(updatedcateg,{where:{id:id}})
+           res.status(200).json(updatedcategory)
+
+        }
+    
+    catch (error) {
+        res.status(400).json(err||{message:"something went wrong"})
+    }
+}
+
+exports.deletecateg=async(req,res)=>{
+    try{
+        if(!req.isAdmin && !req.isVerified)
+        {
+            res.status(400).json({message:"Unauthorised Access"})
+        }
+        var id=req.params.id
+
+        await category.destroy({where:{id:id}})
+        res.status(200).json({message:"deletion success"})
+    }
+    catch(err)
+    {
+        res.status(400).json(err||{message:"something went wrong"})
+    }
+}
+exports.deleteallcateg=async(req,res)=>{
+    try{
+        if(!req.isAdmin && !req.isVerified)
+        {
+            res.status(400).json({message:"Unauthorised Access"})
+        }
+
+        await category.truncate()
+        res.status(200).json({message:"deletion success"})
+}
+catch(err)
+{
+    res.status(400).json(err||{message:"something went wrong"})
+}
+}
--- Routes/Categoryroutes.js
+++ Routes/Categoryroutes.js
@@ -2,5 +2,10 @@ const cc=require("../Controller/Categorycontroller")
 const mw=require("../Middleware/authmiddleware")
 
 module.exports=(app)=>{
-    app.post("/foodapp/v1/createcategoty",mw.authvalidator,cc.createcategory)
+    app.post("/foodapp/v1/createcategory",mw.authvalidator,cc.createcategory)
+    app.get("/foodapp/v1/getallcategory",cc.getallcategories)
+    app.get("/foodapp/v1/getcategory/:name",cc.getonecategory)
+    app.put("/foodapp/v1/updatecategory/:id",mw.authvalidator,cc.updatecateg)
+    app.delete("/foodapp/v1/deletecategory/:id",mw.authvalidator,cc.deletecateg)
+    app.delete("/foodapp/v1/deletecategory",mw.authvalidator,cc.deleteallcateg)
 }
